4d24853a8e671020d94aa095961264c80f449e6f
Pablo Avila <avila@oakland.edu>
2024-01-24 21:32:52 -0500

    First commit

--- JavaMetricConverter.java
+++ JavaMetricConverter.java
@@ -4,7 +4,7 @@ public class JavaMetricConverter {
     public static void main(String[] args) {
         int integer;
         double result;
-        int response;
+        int response=0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the metric converter! Input 1 for kg to lbs. Input 2 for km to m. Input 3 for mm to in. Input 4 for grams to ounces. Input 5 to exit program.");
         while (response != 5) {
@@ -16,32 +16,33 @@ public class JavaMetricConverter {
             System.out.println("Input kg: ");
             integer = scanner.nextInt();
             result = (integer*2.20462);
-            System.out.println(result);
+            System.out.println(result + " pounds");
             break;
             case 2:
             System.out.println("Input km: ");
             integer = scanner.nextInt();
             result = (integer*1000);
-            System.out.println(result);
+            System.out.println(result+ " meters");
             break;
             case 3:
             System.out.println("Input mm: ");
             integer = scanner.nextInt();
             result = (integer*0.0393701);
-            System.out.println(result);
+            System.out.println(result+ " inches");
             break;
             case 4:
             System.out.println("Input grams: ");
             integer = scanner.nextInt();
             result = (integer*0.0393701);
-            System.out.println(result);
+            System.out.println(result+ " ounces");
             break;
             case 5:
             System.out.println("Terminating system. ");
+            System.exit(0);
             default:
             System.out.println("Please input a valid response");
         }
         }
-        java.util.Scanner.close()
+        
     }
 }
---
+++ ReverseAString.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char ch;
+        System.out.println("Type in a string to be reversed: ");
+        String input= scanner.nextLine();
+        
+        for (int i = input.length() - 1; i >= 0; i--) {
+            ch=input.charAt(i);
+            System.out.print(ch);
+            
+        }
+        
+    }
+
+
+
+
+
+    }
